6c815e4c228d1eb3bc2aab09793be3f89ad9fd1a
LeeJeHee <32095533+MoonPhile@users.noreply.github.com>
2021-11-10 14:23:29 +0900

    Add files via upload
    add motor3

--- braille_arduino.ino
+++ braille_arduino.ino
@@ -8,12 +8,14 @@ int now2=0;
 int next2=0;
 String temp2;
 String braille[2];
-int steps[8]={0,256,512,768,1024,1280,1536,1792};
-Stepper myStepper(2048,25,23,24,22);
-Stepper myStepper2(2048,29,27,28,26);
+int steps[8]={0,256,512,768,1024,1280,1536,1792}; //각도배열
+Stepper myStepper(2048,25,23,24,22); //모터1
+Stepper myStepper2(2048,29,27,28,26); //모터2
+Stepper myStepper3(2048,33,31,32,30); //모터3
 
 boolean motor1 = true;
 boolean motor2 = false;
+boolean motor3 = false;
  
 #define BT_RXD 52
 #define BT_TXD 53
@@ -24,7 +26,8 @@ void setup(){
   Serial.begin(9600);
   bluetooth.begin(9600);
   myStepper.setSpeed(14); 
-  myStepper2.setSpeed(14); 
+  myStepper2.setSpeed(14);
+  myStepper3.setSpeed(14); 
 }
  
 void loop(){
@@ -40,7 +43,7 @@ void loop(){
       Serial.print("번째:");
       Serial.println(braille[i]);
     }*/
-  if(motor1==true){
+  if(motor1==true){ //모터1
     next = temp.toInt();
     Serial.print("next:");
     Serial.println(next);
@@ -61,10 +64,10 @@ void loop(){
     Serial.print("now:");
     Serial.println(now);
     Serial.println("끝");
-    motor1=false; //모터1끝
-    motor2=true; //모터2 켬
+    motor1=false; //모터1 off
+    motor2=true; //모터2 on
   delay(500);
-  }else if(motor2==true){//두번째모터
+  }else if(motor2==true){//모터2
     next = temp.toInt();
     Serial.print("next:");
     Serial.println(next);
@@ -80,14 +83,37 @@ void loop(){
       Serial.println("모터2 반대방향 회전");
       myStepper2.step(steps[next]);
     }   
+    now2 = next%8; //현재값 저장
+    Serial.print("now2:");
+    Serial.println(now2);
+    Serial.println("끝");
+    motor2=false; //모터2 off
+    motor3=true; //모터3 on
+  }else if(motor3==true){//모터3
+    next = temp.toInt();
+    Serial.print("next:");
+    Serial.println(next);
+    
+    next = next - now2;  //다음 칸 계산
+    Serial.print("계산후next:");
+    Serial.println(next);
+    
+    if(next>=0){  //모터 회전
+    Serial.println("모터3 정방향 회전");
+    myStepper2.step(steps[next]);
+    }else{
+      Serial.println("모터3 반대방향 회전");
+      myStepper2.step(-steps[next]);
+    }   
     
     now2 = next%8; //현재값 저장
     Serial.print("now2:");
     Serial.println(now2);
     Serial.println("끝");
-    motor2=false;
-    motor1=true;
-  }
+    motor3=false; //모터3 off
+    motor1=true; //모터1 on
+    }
+  
   }//블루투스 연결 여기까지
   
   if (Serial.available()) {
